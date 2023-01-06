c83ee9c7c3fa6f4932835c8b69d2d88c2d378d50
geon00 <jgn159753@naver.com>
2023-01-06 09:31:48 +0900

    first commit

---
+++ src/test/java/com/jingu/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingu.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
